7f5086ccc5235ec1a98d6b5f229ebbb4d8ee059d
lpq <lanxiu0523@gmail.com>
2024-03-22 15:55:44 +0800

    “v1.0.3”

--- yolov3/detect.py
+++ yolov3/detect.py
@@ -122,8 +122,8 @@ if __name__ == '__main__':
     parser.add_argument('--weights', type=str, default='weights/yolov3-tiny.weights', help='path to weights file')
     parser.add_argument('--images', type=str, default='data/samples', help='path to images')
     parser.add_argument('--img-size', type=int, default=256, help='size of each image dimension')
-    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
-    parser.add_argument('--nms-thres', type=float, default=0.5, help='iou threshold for non-maximum suppression')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--nms-thres', type=float, default=0.25, help='iou threshold for non-maximum suppression')
     opt = parser.parse_args()
     print(opt)
 
